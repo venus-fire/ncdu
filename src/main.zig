74c91768a0d98962801a3c6e4b3a3d197b249cf6
Yorhel <git@yorhel.nl>
2025-08-16 11:16:53 +0200

    Version 2.9

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Yorhel <projects@yorhel.nl>
 // SPDX-License-Identifier: MIT
 
-pub const program_version = "2.8.2";
+pub const program_version = "2.9";
 
 const std = @import("std");
 const model = @import("model.zig");
